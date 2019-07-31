cfd0d13598845136b5a10eca8471616fc4296fb0
okexi <eikv@qq.com>
2019-07-31 22:06:45 +0800

    添加参数调用

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # videoProcess
----
-主要功能：后期视频压制avs脚本的生成
+主要功能：后期视频压制时简单avs脚本的生成
 ### 目前的功能
 运行实例：
 >![运行结果](./Pic/Res.png)
 
+<<<<<<< HEAD
+加入环境变量后支持参数调用，在视频目录下直接输入文件名更方便
+=======
 输入的文件路径为 
 > G:\video\[sucai]\核爆神曲《ALIEZ》现场版\《ALIEZ》 现场版.mp4
+>>>>>>> 4b5e83dab3fa5425648b5a3a80667795686e1d74
 
 生成的`《ALIEZ》 现场版.avs`中的内容
 ```
@@ -15,11 +18,11 @@ LoadPlugin("D:\Programs\MeGUI\tools\avisynth_plugin\VSFilter.dll")
 video=FFVideoSource("G:\video\[sucai]\核爆神曲《ALIEZ》现场版\《ALIEZ》 现场版.mp4").LanczosResize(1280,720).TextSub("G:\video\[sucai]\核爆神曲《ALIEZ》现场版\《ALIEZ》 现场版.ass")
 audio=FFAudioSource("G:\video\[sucai]\核爆神曲《ALIEZ》现场版\《ALIEZ》 现场版.mp4")
 AudioDub(video,audio)
-trim(234,324)
+trim(23,546)
 
 ```
-只适用于在`简体中文windows`环境下，编码方式为`GBK`，相信能满足一部分人的后期需求，`LanczosResize`更改分辨率，当前默认更改为720p,`TextSub`添加字幕（字幕需要在同文件夹下，与视频同名），`trim(234,324)` 裁取 234-324 帧。
+只适用于在`简体中文windows`环境下，编码方式为`GBK`，相信能满足一部分人的后期需求，`LanczosResize`更改分辨率，当前默认更改为720p,`TextSub`添加字幕（字幕需要在同文件夹下，与视频同名），`trim(23,546)` 裁取 23-546 帧。
 
 ### 使用方式
-配合MEGUI使用，直接将生成的avs脚本拖入即可
+配合MEGUI使用，直接将生成的avs脚本拖入即可，需要注意修改下视频输出文件名，防止覆盖原视频
 ![使用脚本](./Pic/shiyong.png)
--- video.cpp
+++ video.cpp
@@ -1,63 +1,68 @@
-#include<iostream>
-#include<fstream>
-#include<stdio.h>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <direct.h>
+#include <string>
 using namespace std;
 
-string getName(string a){ 
-    string name,na;
-     int pos=a.size();
-     for(;a[--pos]!=L'.';){}
-     for(int i=pos-1;a[i]!='\\';i--){
-       name.push_back(a[i]);
-   }
-   for(;!name.empty();){
-       na.push_back(name.back());
-       name.pop_back();
-   }
-   return na;
+string getName(string a)
+{
+    return a.substr(a.find_last_of('\\') + 1);
 }
-string quchu(string a){
-    string res;
-     int pos=a.size();
-     for(;a[--pos]!=L'.';){}
-    for(int i=0;i<pos;i++){
-        res.push_back(a[i]);
+string quchu(string a)
+{
+    return a.substr(0, a.find_last_of('.'));
+}
+int main(int argc, char *argv[])
+{
+    const string load = "LoadPlugin(\"D:\\Programs\\MeGUI\\tools\\ffms\\ffms2.dll\")\nLoadPlugin(\"D:\\Programs\\MeGUI\\tools\\avisynth_plugin\\VSFilter.dll\")";
+    string n1, name, origin;
+    if (argc > 1)
+    {
+        n1+=argv[1];
+        for(int i=2;i<argc;i++)
+        n1 =n1+' '+argv[i];
+        name = quchu(n1);
+        char pwd[256];
+        getcwd(pwd, 256);
+        origin = pwd;
+        origin = origin + '\\' + n1;
     }
-    return res;
-}
-int main(){
-    const string load="LoadPlugin(\"D:\\Programs\\MeGUI\\tools\\ffms\\ffms2.dll\")\nLoadPlugin(\"D:\\Programs\\MeGUI\\tools\\avisynth_plugin\\VSFilter.dll\")";
-    cout<<"请输入完整的文件路径：\n";
-    string origin;
-    getline(cin,origin);
-    string name=getName(origin);
-     ofstream fout;
-    fout.open(name+".avs");
-        fout<<load<<endl;
-        fout<<"video=FFVideoSource(\""<<origin<<"\")";
-        cout<<"是否更改尺寸：1-更改、不更改（默认）,请输入：";
-        char isResize=0;
-        cin>>isResize;
-        if(isResize=='1'){
-            fout<<".LanczosResize(1280,720)";
-        }
-        cout<<"是否添加字幕：1-添加、不添加（默认）,请输入：";
-        cin>>isResize;
-        if(isResize=='1')fout<<".TextSub(\""<<quchu(origin)<<".ass\")";
-        fout<<endl;
-        fout<<"audio=FFAudioSource(\""<<origin<<"\")"<<endl;
-        fout<<"AudioDub(video,audio)"<<endl;
-        cout<<"是否切割视频：1-切割、不切割（默认）,请输入：";
-        cin>>isResize;
-        if(isResize=='1'){
-            cout<<"请分别输入起始帧和结束帧：";
-            int st,en;
-            cin>>st>>en;
-            fout<<"trim("<<st<<","<<en<<")"<<endl;
-        }
-        fout.close();
-        cout<<"已经在当前目录创建"<<name<<".avs\n";
-        system("pause");
+    else
+    {
+        cout << "请输入完整的文件路径：\n";
+        getline(cin, origin);
+        name = quchu(getName(origin));
+    }
+    ofstream fout;
+    fout.open(name + ".avs");
+    fout << load << endl;
+    fout << "video=FFVideoSource(\"" << origin << "\")";
+    cout << "是否更改尺寸：1-更改、不更改（默认）,请输入：";
+    char isResize = 0;
+    cin >> isResize;
+    if (isResize == '1')
+    {
+        fout << ".LanczosResize(1280,720)";
+    }
+    cout << "是否添加字幕：1-添加、不添加（默认）,请输入：";
+    cin >> isResize;
+    if (isResize == '1')
+        fout << ".TextSub(\"" << quchu(origin) << ".ass\")";
+    fout << endl;
+    fout << "audio=FFAudioSource(\"" << origin << "\")" << endl;
+    fout << "AudioDub(video,audio)" << endl;
+    cout << "是否切割视频：1-切割、不切割（默认）,请输入：";
+    cin >> isResize;
+    if (isResize == '1')
+    {
+        cout << "请分别输入起始帧和结束帧：";
+        int st, en;
+        cin >> st >> en;
+        fout << "trim(" << st << "," << en << ")" << endl;
+    }
+    fout.close();
+    cout << "已经在当前目录创建" << name << ".avs\n";
+    system("pause");
     return 0;
 }
